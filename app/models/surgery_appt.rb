536d7b755fd96f38ee78d2fdcbb197370be9a624
Christopheraarguello <christopheraarguello@gmail.com>
2017-10-06 11:54:47 -0400

    pushing basic inventory collection equation to github, equation is still in draft and not yet complete, only extracts one record at a time

--- app/models/surgery_appt.rb
+++ app/models/surgery_appt.rb
@@ -5,4 +5,36 @@ class SurgeryAppt < ApplicationRecord
 
   validates :surgery_date, presence: true
 
+  # def demand
+  #   @appts = SurgeryAppt.all
+  #   @appts.each do |list|
+  #     list.surgery_type
+  #   end
+  # end
+
+  def appts
+    @appts = SurgeryAppt.all
+  end
+
+
+  # This gets the individual supply list items,
+  #I am still not able to extract the qty's at this moment
+  # SurgeryAppt.all.each do |surgery|
+  # p surgery.surgery_type.surgery_recipe_reqs.where(supply_list_id:1)
+  # end
+
+  # This gets the individual supply list items and their quantity
+  #this is only working for 1 item at a time
+  #next step is to assign variable "i" to supply list id
+  # arr = []
+  # SurgeryAppt.all.each do |surgery|
+  #   surgery.surgery_type.surgery_recipe_reqs.where(supply_list_id:1).each do |item|
+  #     p arr << item.qty
+  #     arr.sum
+  #   end
+  #   p arr.sum
+  # end
+
+
+
 end
