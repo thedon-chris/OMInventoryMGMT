d6c96a04731215c3505cf8d94384e46f7329c431
Christopheraarguello <christopheraarguello@gmail.com>
2017-10-10 00:23:51 -0400

    created user model, and sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'session/new'
+
+  get 'session/create'
+
+  get 'session/destroy'
+
+  resources :users
   resources :inventories
   resources :actual_recipe_reqs
   resources :surgery_appts
