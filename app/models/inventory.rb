968e28c3a7a0f7873beb1169e2e5666c9908b5a1
Sam Abbassi <samabbassi1@gmail.com>
2017-10-12 18:20:28 -0400

    fixed expiration method in Inventory model

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -36,13 +36,14 @@ class Inventory < ApplicationRecord
   def expiration
     qty = 0
     item_number = self.supply_list_id
-    self.clinic.surgery_appts.where('surgery_date > ?', Date.today).each do |appt|
-      appt.surgery_recipe_reqs.where(supply_list_id:item_number).each do |req|
+    self.clinic.surgery_appts.where(complete:false).where('surgery_date > ?', Date.today).each do |appt|
+      appt.surgery_type.surgery_recipe_reqs.where(supply_list_id:item_number).each do |req|
         qty += req.qty
       end
     end
     qty
   end
+
 
 
 end
